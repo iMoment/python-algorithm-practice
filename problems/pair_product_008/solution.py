91952c149e47a941a6d178a5ed00d62f5cd645a8
Stanley Pan <imoment.developer@gmail.com>
2025-06-28 14:14:30 +0900

    add pair_product problem, edit readme

---
+++ problems/pair_product_008/solution.py
@@ -0,0 +1,46 @@
+"""
+Write a function, pair_product, that takes in a list and a target product as arguments. The function should return a tuple containing a pair of indices whose elements multiply to the given target. 
+
+The indices returned must be unique. Be sure to return the indices, not the elements themselves.
+
+There is guaranteed to be one such pair whose product is the target.
+"""
+
+# Function name: pair_product
+# Input(s): list, int
+# Expected output(s): tuple
+
+# Write your code below this line
+def pair_product(numbers, target_product):
+  previous_numbers = {}
+  
+  for index, num in enumerate(numbers):
+    complement = target_product / num
+    
+    if complement in previous_numbers:
+      return (previous_numbers[complement], index)
+    
+    previous_numbers[num] = index
+
+"""
+Complexity:
+n = length of numbers list
+
+Time: O(n)
+Space: O(n)
+"""
+
+# Remark: This is a brute-force alternative solution that trades time for space
+def pair_product(numbers, target_product):
+    for i in range(0, len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] * numbers[j] == target_product:
+                return (i, j)
+            
+"""
+Complexity:
+n = length of numbers list
+
+Time: O(n^2)
+Space: O(1)
+"""
---
+++ problems/pair_product_008/test.py
@@ -0,0 +1,41 @@
+from pair_product import *
+
+test_cases = [
+    ([3, 2, 5, 4, 1], 8, (1, 3)),
+    ([3, 2, 5, 4, 1], 10, (1, 2)),
+    ([4, 7, 9, 2, 5, 1], 5, (4, 5)),
+    ([4, 7, 9, 2, 5, 1], 35, (1, 4)),
+    ([3, 2, 5, 4, 1], 10, (1, 2)),
+    ([4, 6, 8, 2], 16, (2, 3)),
+]
+
+def test(input1, input2, expected_output):
+    print("---------------------------------")
+    print(f"Input(s): {input1}, {input2}")
+    print(f"Expected output: {expected_output}")
+    result_output = pair_product(input1, input2)
+    print(f"Actual output: {result_output}")
+
+    if result_output == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+if __name__ == "__main__":
+    main()
